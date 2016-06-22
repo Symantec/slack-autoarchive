374e0c83e43fbd3b1466b71faad23d76252e6ef2
John Theodore <JohnTheodore@github.com>
2016-06-21 22:56:13 -0700

    add functionality that writes an audit log file, each archived channel gets an entry in the file

--- slack-autoarchive.py
+++ slack-autoarchive.py
@@ -2,19 +2,20 @@
 
 from datetime import timedelta, datetime
 import os
-import sys
 import requests
+import sys
 
 #
 # This will archive inactive channels. The inactive period is in days as 'DAYS_INACTIVE'
 # You can put this in a cron job to run daily to do slack cleanup.
 #
 
-SLACK_TOKEN      = os.getenv('SLACK_TOKEN')
-DAYS_INACTIVE    = 60
-TOO_OLD_DATETIME = datetime.now() - timedelta(days=DAYS_INACTIVE)
-DRY_RUN = (os.getenv('DRY_RUN', "true") == "true")
-ADMIN_CHANNEL = os.getenv('ADMIN_CHANNEL')
+ADMIN_CHANNEL      = os.getenv('ADMIN_CHANNEL')
+AUDIT_LOG          = 'audit.log'
+DAYS_INACTIVE      = 60 
+DRY_RUN            = (os.getenv('DRY_RUN', "true") == "true")
+SLACK_TOKEN        = os.getenv('SLACK_TOKEN')
+TOO_OLD_DATETIME   = datetime.now() - timedelta(days=DAYS_INACTIVE)
 WHITELIST_KEYWORDS = os.getenv('WHITELIST_KEYWORDS')
 
 
@@ -68,6 +69,8 @@ def get_inactive_channels(all_unarchived_channels, too_old_datetime):
         inactive_channels.append(channel)
   return inactive_channels
 
+
+# If you add channels to the WHITELIST_KEYWORDS constant they will be exempt from archiving.
 def filter_out_whitelist_channels(inactive_channels):
     channels_to_archive = []
     for channel in inactive_channels:
@@ -80,10 +83,16 @@ def filter_out_whitelist_channels(inactive_channels):
         channels_to_archive.append(channel)
     return channels_to_archive
 
+
 def send_channel_message(channel_id, message):
   payload  = {'channel': channel_id, 'username': 'channel_reaper', 'icon_emoji': ':ghost:', 'text': message}
   api_endpoint = 'chat.postMessage'
   slack_api_http_get(api_endpoint=api_endpoint, payload=payload)
+
+
+def write_log_entry(file_name, entry):
+  with open(file_name, 'a') as logfile:
+    logfile.write(entry + '\n')
 
 
 def archive_inactive_channels(channels):
@@ -91,20 +100,22 @@ def archive_inactive_channels(channels):
   api_endpoint = 'channels.archive'
   for channel in channels:
     if not DRY_RUN:
-      message = "This channel has had no activity for %s days. It is being auto-archived." % DAYS_INACTIVE
-      message += " If you feel this is a mistake you can <https://slack.com/archives/archived|unarchive this channel> to bring it back at any point."
-      send_channel_message(channel['id'], message)
+      channel_message = "This channel has had no activity for %s days. It is being auto-archived." % DAYS_INACTIVE
+      channel_message += " If you feel this is a mistake you can <https://slack.com/archives/archived|unarchive this channel> to bring it back at any point."
+      send_channel_message(channel['id'], channel_message)
       if ADMIN_CHANNEL:
         send_channel_message(ADMIN_CHANNEL, "Archiving channel... %s" % channel['name'])
-      payload = {'channel': channel['id']}
+      payload        = {'channel': channel['id']}
+      stdout_message = "Archiving channel... %s" % channel['name']
+      log_message    = str(datetime.now()) + ' ' + stdout_message
       slack_api_http_get(api_endpoint=api_endpoint, payload=payload)
+      write_log_entry(AUDIT_LOG, log_message)
 
-    print "Archiving channel... %s" % channel['name']
+    print stdout_message
 
 if DRY_RUN:
   print "THIS IS A DRY RUN. NO CHANNELS ARE ACTUALLY ARCHIVED."
 all_unarchived_channels = get_all_channels()
 inactive_channels       = get_inactive_channels(all_unarchived_channels, TOO_OLD_DATETIME)
-channels_to_archive = filter_out_whitelist_channels(inactive_channels)
+channels_to_archive     = filter_out_whitelist_channels(inactive_channels)
 archive_inactive_channels(channels_to_archive)
-
